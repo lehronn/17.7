f25d8a0e1bbd5231e4944edf622d42445ceda2ce
Mateusz Stomski <lehronn@gmail.com>
2017-11-12 14:49:30 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '970874201692-20unrkrc9f1agjihp3798vovjupn9pvp.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'u5JfEcM-WQbN0WjOBHfucZ1Q',
+  'CALLBACK_URL': 'http://www.localhost:3000/auth/google/callback'
+};
+
+
+
